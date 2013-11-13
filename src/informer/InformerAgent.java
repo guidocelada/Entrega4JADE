93237a1b34f6b58da77829096341e0273df1dddd
guidocelada <celadaguido@gmail.com>
2013-11-12 23:56:47 -0300

    ej1

--- src/informer/InformerAgent.java
+++ src/informer/InformerAgent.java
@@ -1,6 +1,5 @@
 package informer;
 
-
 import jade.core.Agent;
 import jade.core.ContainerID;
 import jade.core.Location;
@@ -19,6 +18,8 @@ public class InformerAgent extends Agent {
     
     private static int mb = 1024*1024;
     
+    private static int PERIOD = 10000; //10 seconds
+    
     private String[] containers = { "Container-1", "Container-2" }; //the containers to move to
        
     private HashMap<String,Integer> results;
@@ -27,6 +28,8 @@ public class InformerAgent extends Agent {
     
     private int iterator;
     
+    private long startTime;
+    
     
     private void initialize() {
         this.iterator = 0;
@@ -34,26 +37,36 @@ public class InformerAgent extends Agent {
         results.put("jvmMemory", 0);
         results.put("processingLoad", 0);
         results.put("totalMemory", 0);
+        this.startTime = System.currentTimeMillis();
     }
     
     @Override
     public void setup() {
-        System.out.println("\nDEBUG: Initiating " + this.getLocalName());
+        System.out.println("\n---------------------------------------\n");
+        System.out.println("DEBUG: Initiating " + this.getLocalName());
         
         initialize();
         this.origin = this.here();
         this.doMove(new ContainerID(containers[iterator], null));     
     }
     
+    
+    /**
+     * Called after the object has been moved to another container
+     **/
     @Override
     public void afterMove() {      
         
         if (this.here().getID().equals(this.origin.getID())) {
-            System.out.println("Total JVM Memory: " + results.get("jvmMemory") + "mb \n");
-            this.doDelete();
+            System.out.printf("\033[1m\nTotal JVM Memory: " + results.get("jvmMemory") + "mb \033[0m \n\n");
+            System.out.printf("\033[1m\nTime that took doing the 'tour': " + (System.currentTimeMillis() - this.startTime) + "ms \033[0m \n\n");
+            this.doWait(PERIOD);
+            initialize();
+            this.doMove(new ContainerID(containers[iterator], null)); 
         } else {
+            
             System.out.println("DEBUG: " + this.getLocalName() + " passing through " + this.here().getName());
-            System.out.println("DEBUG: " + this.getLocalName() + " JVM memory is: " + (Runtime.getRuntime().totalMemory() / mb) + " mb");
+            System.out.println("DEBUG: " + this.getLocalName() + " JVM memory is: " + (Runtime.getRuntime().freeMemory() / mb) + " mb");
 
             int auxMemory = (int) (results.get("jvmMemory") + (Runtime.getRuntime().totalMemory() / mb));
             results.put("jvmMemory", auxMemory); 
@@ -63,10 +76,5 @@ public class InformerAgent extends Agent {
             else 
                 this.doMove(this.origin);
         }
-    }
-    
-    @Override
-    public void takeDown() {
-        System.out.println("DEBUG: Taking down " + this.getLocalName() + " at " + this.here().getName());
     }
 }
