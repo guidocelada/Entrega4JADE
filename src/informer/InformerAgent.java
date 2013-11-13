b75f13c8036b429d6719da1803fc92201092585c
guidocelada <celadaguido@gmail.com>
2013-11-12 21:47:34 -0300

    ej1

--- src/informer/InformerAgent.java
+++ src/informer/InformerAgent.java
@@ -2,7 +2,9 @@ package informer;
 
 
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
+import jade.core.ContainerID;
+import jade.core.Location;
+import java.util.HashMap;
 
 /**
  * This is a mobile agent that moves between containers and informs each X
@@ -15,19 +17,56 @@ import jade.core.behaviours.Behaviour;
 @SuppressWarnings("serial")
 public class InformerAgent extends Agent {  
     
-    @Override
-    public void setup() {
-        System.out.println("\nDEBUG: Initiating " + this.getLocalName());
-        addBehaviour(new InformerBehaviour(this));
+    private static int mb = 1024*1024;
+    
+    private String[] containers = { "Container-1", "Container-2" }; //the containers to move to
+       
+    private HashMap<String,Integer> results;
+    
+    private Location origin;
+    
+    private int iterator;
+    
+    
+    private void initialize() {
+        this.iterator = 0;
+        results = new HashMap<>();
+        results.put("jvmMemory", 0);
+        results.put("processingLoad", 0);
+        results.put("totalMemory", 0);
     }
     
     @Override
-    public void afterMove() {
-        System.out.println("DEBUG: " + this.getLocalName() + " passing through " + this.here().getName());
+    public void setup() {
+        System.out.println("\nDEBUG: Initiating " + this.getLocalName());
+        
+        initialize();
+        this.origin = this.here();
+        this.doMove(new ContainerID(containers[iterator], null));     
+    }
+    
+    @Override
+    public void afterMove() {      
+        
+        if (this.here().getID().equals(this.origin.getID())) {
+            System.out.println("Total JVM Memory: " + results.get("jvmMemory") + "mb \n");
+            this.doDelete();
+        } else {
+            System.out.println("DEBUG: " + this.getLocalName() + " passing through " + this.here().getName());
+            System.out.println("DEBUG: " + this.getLocalName() + " JVM memory is: " + (Runtime.getRuntime().totalMemory() / mb) + " mb");
+
+            int auxMemory = (int) (results.get("jvmMemory") + (Runtime.getRuntime().totalMemory() / mb));
+            results.put("jvmMemory", auxMemory); 
+            this.iterator++;
+            if (iterator < this.containers.length) 
+                this.doMove(new ContainerID(containers[iterator], null));
+            else 
+                this.doMove(this.origin);
+        }
     }
     
     @Override
     public void takeDown() {
-        System.out.println("DEBUG: Taking down " + this.getLocalName() + " on location " + this.here().getName());
+        System.out.println("DEBUG: Taking down " + this.getLocalName() + " at " + this.here().getName());
     }
 }
